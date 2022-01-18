317b7c2fff189f9f540fab37fa0a775f3ea52e37
Gavin Dold <gdold42@gmail.com>
2022-01-18 22:34:05 +0000

    Fix whitespace in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name = "WordleBot",
     version = "0.2",
     author='Gavin Dold',
-	description='This bot solves wordles.',
-	url='https://github.com/gdold/WordleBot',
+    description='This bot solves wordles.',
+    url='https://github.com/gdold/WordleBot',
     license='The MIT License (MIT)',
     packages = find_packages(exclude=['*test']),
-    #scripts = ['scripts/wordlebot'],
     install_requires = ['argparse','numpy','pandas'],
-	entry_points={ 'console_scripts': ['wordlebot=WordleBot:run_bot_from_cmd'] },
-	python_requires=">=3.6"
+    #scripts = ['scripts/wordlebot'], # depreciated in favour of console_scripts
+    entry_points={ 'console_scripts': ['wordlebot=WordleBot:run_bot_from_cmd'] },
+    python_requires=">=3.6"
 )
