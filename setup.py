b2910934bd965f98442c50ac54176a8c16fa9c70
Gavin Dold <gdold42@gmail.com>
2022-02-21 20:52:51 +0000

    Update release to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "wordlesolver",
-    version = "0.2",
+    version = "0.2.1",
     author='Gavin Dold',
     description='This bot solves wordles.',
     url='https://github.com/gdold/WordleBot',
